a40ab60f1ddc3441a48477d6f2c48035c5be51a7
pat-s <patrick.schratz@gmail.com>
2020-03-17 17:23:33 +0100

    use hugo lithiumt heme

--- tic.R
+++ tic.R
@@ -5,15 +5,5 @@ get_stage("install") %>%
   add_step(step_install_cran("blogdown")) %>%
   add_code_step(blogdown::install_hugo())
 
-if (Sys.getenv("id_rsa") != "") {
-  get_stage("before_deploy") %>%
-    add_step(step_setup_ssh()) %>%
-    add_step(step_setup_push_deploy(
-      path = "public",
-      branch = "gh-pages"
-    ))
-
-  get_stage("deploy") %>%
-    add_code_step(blogdown::build_site()) %>%
-    add_step(step_do_push_deploy(path = "public"))
-}
+get_stage("script") %>%
+  add_code_step(blogdown::build_site())
