8e093f2e8634bb3772eab61f34b4d81792557c1f
Patrick Schratz <patrick.schratz@gmail.com>
2020-03-17 17:43:38 +0100

    Use hugo-lithium theme to prevent vulnerabilities, update README (#7)

--- tic.R
+++ tic.R
@@ -1,15 +1,7 @@
-get_stage("before_install") %>%
-  add_code_step(update.packages(ask = FALSE))
-
 get_stage("install") %>%
+  add_step(step_install_deps()) %>%
   add_step(step_install_cran("blogdown")) %>%
   add_code_step(blogdown::install_hugo())
 
-if (Sys.getenv("id_rsa") != "") {
-  get_stage("before_deploy") %>%
-    add_step(step_setup_ssh())
-
-  get_stage("deploy") %>%
-    add_code_step(blogdown::build_site()) %>%
-    add_step(step_push_deploy())
-}
+get_stage("script") %>%
+  add_code_step(blogdown::build_site())
