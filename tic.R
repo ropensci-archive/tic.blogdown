f3df147e13f056cceb47bb412d63111215d34504
pat-s <patrick.schratz@gmail.com>
2020-03-25 18:59:49 +0100

    first setup push deploy, then build site, then deploy

--- tic.R
+++ tic.R
@@ -3,17 +3,19 @@ get_stage("install") %>%
   add_step(step_install_cran("blogdown")) %>%
   add_code_step(blogdown::install_hugo())
 
-get_stage("script") %>%
+get_stage("deploy") %>%
   add_code_step(blogdown::build_site())
 
 if (ci_on_travis() && ci_has_env("BUILD_BLOGDOWN")) {
   get_stage("before_deploy") %>%
-    add_step(step_setup_ssh())
-
-  if (ci_get_branch() == "master") {
-    get_stage("deploy") %>%
-      add_step(step_setup_push_deploy(path = "public", branch = "gh-pages",
-        orphan = TRUE)) %>%
-      add_step(step_do_push_deploy(path = "public"))
-  }
+    add_step(step_setup_ssh()) %>%
+    add_step(step_setup_push_deploy(
+      path = "public",
+      branch = "gh-pages",
+      orphan = TRUE
+    )) %>%
+    if (ci_get_branch() == "master") {
+      get_stage("deploy") %>%
+        add_step(step_do_push_deploy(path = "public"))
+    }
 }
