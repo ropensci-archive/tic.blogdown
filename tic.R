f49369287f6c3fa14ae8beb6aa86ed8a14a7ac97
pat-s <patrick.schratz@gmail.com>
2020-03-25 18:43:20 +0100

    push site to gh-pages branch, condition on BUILD_BLOGDOWN env var

--- tic.R
+++ tic.R
@@ -5,3 +5,15 @@ get_stage("install") %>%
 
 get_stage("script") %>%
   add_code_step(blogdown::build_site())
+
+if (ci_on_travis() && ci_has_env("BUILD_BLOGDOWN")) {
+  get_stage("before_deploy") %>%
+    add_step(step_setup_ssh())
+
+  if (ci_get_branch() == "master") {
+    get_stage("deploy") %>%
+      add_step(step_setup_push_deploy(path = "docs", branch = "gh-pages",
+        orphan = TRUE)) %>%
+      add_step(step_do_push_deploy(path = "docs"))
+  }
+}
