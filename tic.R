6826b827367b61c18bdd8323099d353fea5cb6b7
pat-s <patrick.schratz@gmail.com>
2020-03-29 19:58:14 +0200

    test macro blogdown

--- tic.R
+++ tic.R
@@ -1,21 +1,3 @@
-get_stage("install") %>%
-  add_step(step_install_deps()) %>%
-  add_step(step_install_cran("blogdown")) %>%
-  add_code_step(blogdown::install_hugo())
-
-get_stage("deploy") %>%
-  add_code_step(blogdown::build_site())
-
 if (ci_on_travis() && ci_has_env("BUILD_BLOGDOWN")) {
-  get_stage("before_deploy") %>%
-    add_step(step_setup_ssh()) %>%
-    add_step(step_setup_push_deploy(
-      path = "public",
-      branch = "gh-pages",
-      orphan = TRUE
-    ))
-  if (ci_get_branch() == "master") {
-    get_stage("deploy") %>%
-      add_step(step_do_push_deploy(path = "public"))
-  }
+  do_blogdown()
 }
